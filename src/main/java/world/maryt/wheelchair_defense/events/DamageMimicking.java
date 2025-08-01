598bdc616cb96972bec169ba4254e088cbe28ab9
Isaac Thompson <risinginiris2017@gmail.com>
2025-08-01 22:17:33 +0800

    /kill and void damage is not prevented

--- src/main/java/world/maryt/wheelchair_defense/events/DamageMimicking.java
+++ src/main/java/world/maryt/wheelchair_defense/events/DamageMimicking.java
@@ -1,19 +1,25 @@
 package world.maryt.wheelchair_defense.events;
 
 import net.minecraft.network.chat.Component;
+import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import static world.maryt.wheelchair.Config.protectPlayersAtThisHP;
 import static world.maryt.wheelchair_defense.Config.playerDefenseMethod;
 
 public class DamageMimicking {
-    @SubscribeEvent
+    @SubscribeEvent(priority = EventPriority.LOWEST)
     public static void onLivingDamaged(LivingDamageEvent event) {
         LivingEntity target = event.getEntity();
         // Only apply for players
         if (!(target instanceof Player player)) return;
+
+        // So /kill and void damage is not prevented
+        if (event.getSource().is(DamageTypes.GENERIC_KILL) && event.getAmount() == Float.MAX_VALUE) return;
+        if (player.getY() < -64) return;
 
         // Backup the damage
         float rawDamageAmount = event.getAmount();
